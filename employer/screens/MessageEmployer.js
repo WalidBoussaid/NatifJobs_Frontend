400802a717546207aa2c819e1455bfee738276e4
WalidBoussaid <walidmeknessi@hotmail.com>
2022-09-15 19:19:30 +0200

    ajout fonction pour page message employer

--- employer/screens/MessageEmployer.js
+++ employer/screens/MessageEmployer.js
@@ -1,10 +1,73 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    FlatList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const MessageEmployer = () => {
+    const [data, setData] = useState([]);
+    const [employer, setEmployer] = useState([]);
+    const [candidate, setCandidate] = useState([]);
+    const [offer, setOffer] = useState([]);
+
+    const fetchData = async () => {
+        const tok = await AsyncStorage.getItem("token"); //recupère le token
+
+        const response = await fetch(
+            "http://192.168.0.119:3000/match/allMatchEmployer",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${tok}`, //donne l'autorisation et lui envoi le token
+                },
+            }
+        );
+
+        if (response.ok) {
+            const result = await response.json();
+            setData(result);
+            setEmployer(result.employer);
+            setCandidate(result.candidate);
+            setOffer(result.offer);
+        } else {
+            alert("Pas de message à afficher !");
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
     return (
         <View style={styles.container}>
-            <Text>MessageEmployer</Text>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                    // onPress={() =>
+                    //     navigation.navigate("OfferDetails", {
+                    //         id: item.id,
+                    //     })
+                    // }
+                    >
+                        <View style={styles.offer}>
+                            <View>
+                                <Text style={styles.title}>
+                                    {item.candidate.firstName} -{" "}
+                                    {item.candidate.lastName}
+                                </Text>
+                                <Text style={styles.title}>
+                                    {item.offer.title}
+                                </Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
 };
@@ -15,6 +78,20 @@ const styles = StyleSheet.create({
         backgroundColor: "teal",
         justifyContent: "center",
         alignItems: "center",
+    },
+    offer: {
+        backgroundColor: "white",
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        marginTop: 20,
+        width: 350,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
     },
 });
 
---
+++ employer/screens/MsgDetailsEmployer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MsgDetailsEmployer = () => {
+    return (
+        <View>
+            <Text>MsgDetailsEmployer</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default MsgDetailsEmployer;
