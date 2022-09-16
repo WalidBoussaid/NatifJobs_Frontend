0bad883e5745102b603e47295c8380f1ce1e9886
WalidBoussaid <walidmeknessi@hotmail.com>
2022-09-16 18:23:15 +0200

    creation page rdv et AddRdv coter amployer

---
+++ employer/screens/Rdv.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Rdv = () => {
+    return (
+        <View>
+            <Text>Rdv</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default Rdv;
